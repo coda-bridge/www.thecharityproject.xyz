b65cadedef832b35fdf9dbaab0d8960408fc5633
JYC0413 <juyichen0413@foxmail.com>
2024-04-12 17:10:24 +0800

    add +852(HongKong)

--- component/Account.js
+++ component/Account.js
@@ -32,7 +32,7 @@ class Account extends HTMLElement {
     }
 
     render() {
-        this.phoneReg = {1: /^\d{10}$/, 65: /^([8|9])\d{7}$/, 86: /^1\d{10}$/}
+        this.phoneReg = {1: /^\d{10}$/, 65: /^([8|9])\d{7}$/,852: /^([6|9])\d{7}$/, 86: /^1\d{10}$/}
         this.emailReg = /^[a-zA-Z\d_.-]+@[a-zA-Z\d-]+(\.[a-zA-Z\d-]+)*\.[a-zA-Z\d]{2,6}$/
         this.shadowRoot.innerHTML = `
         <div id="phone" style="margin-top: 2rem" class="form-group">
@@ -70,7 +70,7 @@ class Account extends HTMLElement {
             }
         }
 
-        countrySelector.list = ["+1", "+65", "+86"];
+        countrySelector.list = ["+1", "+65", "+852", "+86"];
         countrySelector.placeholder = ""
         phoneCountry.value = 65
         countrySelector.defaultValue = "+65"
